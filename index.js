c9d982c7cb1a7937cf905443f0096f122aefece6
Tim Williams <timwilliams.tx@gmail.com>
2019-01-24 23:28:33 +0000

    create object and include methods

--- index.js
+++ index.js
@@ -4,28 +4,44 @@ const taskComplete = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logState: function() {
+      console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+    },
+
+    markComplete: function() {
+      task.complete = true;
+    },
+  };
+  return task;
 }
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
+// function completeTask(task) {
+//   task.complete = true;
+// }
 
 // Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
+// function logTaskState(task) {
+//   console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+// }
 
 // DRIVER CODE BELOW
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+// newTask("Clean Cat Litter"); // task 0
+// newTask("Do Laundry"); // task 1
+
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markComplete();
+task1.logState(); // Clean Cat Litter has been completed
+
+console.log(tasks);
